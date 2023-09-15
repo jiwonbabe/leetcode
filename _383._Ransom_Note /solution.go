943e3142a3f72dc6f06c29152d258f8648d7d4e5
jiwonbabe <jwb8705@gmail.com>
2023-09-15 22:09:26 +0900

    383__Ransom_Note_

--- _383._Ransom_Note /solution.go
+++ _383._Ransom_Note /solution.go
@@ -1,19 +1,15 @@
 package _383__Ransom_Note_
 
 func canConstruct(ransomNote string, magazine string) bool {
-	rhm := make(map[byte]int)
 	mhm := make(map[byte]int)
 
-	for i := 0; i < len(ransomNote); i++ {
-		rhm[ransomNote[i]]++
-	}
 	for i := 0; i < len(magazine); i++ {
 		mhm[magazine[i]]++
 	}
 
-	for k, v := range rhm {
-		if mhm[k] >= v {
-			mhm[k] -= v
+	for i := 0; i < len(ransomNote); i++ {
+		if v, _ := mhm[ransomNote[i]]; v > 0 {
+			mhm[ransomNote[i]]--
 		} else {
 			return false
 		}
