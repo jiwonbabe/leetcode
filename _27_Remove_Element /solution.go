8bd7dc082646d8107044395a1177ddfda56155c8
jiwonbabe <jwb8705@gmail.com>
2023-09-23 21:49:45 +0900

    27_Remove_Element_

---
+++ _27_Remove_Element /solution.go
@@ -0,0 +1,13 @@
+package _27_Remove_Element_
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+
+	return i
+}
