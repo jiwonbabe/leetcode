f411db228604c1d9dcb505e43f3af8d0448118d3
jiwonbabe <jwb8705@gmail.com>
2023-12-13 23:11:42 +0900

    226_Invert_Binary_Tree_

---
+++ _226_Invert_Binary_Tree /solution.go
@@ -0,0 +1,20 @@
+package _226_Invert_Binary_Tree_
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
